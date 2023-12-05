985d1c901e21b3cbad0c14ffe7c9ff731c54f6cc
Severino Neto <severino.neto@logiquesistemas.com.br>
2023-12-05 01:28:03 -0300

    #ITS-2 - Resolvendo bug de tipagens no layout home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ReactNode } from 'react'
+import { LayoutProps } from '../../.next/types/app/layout'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -27,7 +28,9 @@ export const metadata: Metadata = {
   ],
 }
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({
+  children,
+}: { children: ReactNode } & LayoutProps) {
   return (
     <html lang="pt-BR">
       <body
